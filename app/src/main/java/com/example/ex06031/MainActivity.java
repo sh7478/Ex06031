b1f1ba24a90b0819d8bfb9974c8477b2c18631c3
sh7478 <sh7478@bs.amalnet.k12.il>
2025-01-07 21:50:24 +0200

    First step

--- app/src/main/java/com/example/ex06031/MainActivity.java
+++ app/src/main/java/com/example/ex06031/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex06031;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textV;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        textV = findViewById(R.id.textV);
+    }
+    public void clicked(View view)
+    {
+        textV.setText("Oh, yea, I've been clicked!");
+        textV.setTextSize(20);
     }
 }
